56bfd94ca4ce6dcf8a7ef3736b1091a42c3ae831
unknown <g.dolcet.jove@gmail.com>
2023-09-27 22:35:32 +0200

    Delete manifest, remove cofyright from footer

--- resources/views/shared/_footer.blade.php
+++ resources/views/shared/_footer.blade.php
@@ -4,7 +4,7 @@
 			<div class="col-12 col-lg-auto mt-3 mt-lg-0">
 				<ul class="list-inline list-inline-dots mb-0">
 					<li class="list-inline-item">
-						Copyright &copy; {{ date('Y') }} INSON, S.A. Todos los derechos reservados.
+
 					</li>
 				</ul>
 			</div>
