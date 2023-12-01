8463c4493c524483ea894ff5b565198f47990710
Guillem Dolcet Jové <g.dolcet.jove@gmail.com>
2023-12-01 17:35:27 +0100

    Changes skills overview

--- resources/views/admin/overview/skills/index.blade.php
+++ resources/views/admin/overview/skills/index.blade.php
@@ -2,7 +2,7 @@
     <div class="card">
         <div class="card-body">
             <div class="row">
-                <div class="d-flex justify-content-between mb-4">
+                <div class="d-flex justify-content-between mb-2">
                     <div><h2 class="page-title">@lang('admin.skills')</h2></div>
                     <div><a href="#"
                             data-controller="remote-modal"
@@ -13,19 +13,23 @@
                         </a></div>
                 </div>
                 @if(!empty($skills))
-                    <div class="d-flex">
+                    <div class="d-flex flex-column">
                         @foreach($skills as $skill)
-                            <div class="d-flex align-items-center">
-                                <div class="col-5"><img src="{{\Storage::url($skill->image)}}" alt="{{$skill->name}}"/></div>
-                                <div class="col-7">
-                                    <div><h4>{{$skill->name}}</h4></div>
-                                    <div>Level: {{$skill->level}}</div>
+                            <div class="d-flex align-items-center justify-content-between p-3">
+                                <div class="col-1 me-4"><img src="{{\Storage::url($skill->image)}}" alt="{{$skill->name}}" width="32" height="32"/></div>
+                                <div class="d-flex flex-column col-10">
+                                    <div class="mb-2"><b>{{$skill->name}}</b></div>
+                                    <div class="progress">
+                                        <div class="progress-bar" style="width: {{$skill->level}}%" role="progressbar" aria-valuenow="{{$skill->level}}" aria-valuemin="0" aria-valuemax="100" aria-label="{{$skill->level}}% Complete">
+                                            <span class="visually-hidden">{{$skill->level}}%</span>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         @endforeach
                     </div>
                 @else
-                    <div>No skills</div>
+                    <div class="text-red">Empty</div>
                 @endif
             </div>
         </div>
