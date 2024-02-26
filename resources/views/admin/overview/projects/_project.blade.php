c0d9f0331686de70f25cd4f707fb681f1ad58c02
Guillem <g.dolcet.jove@gmail.com>
2024-02-26 18:55:33 +0100

    Changes back

--- resources/views/admin/overview/projects/_project.blade.php
+++ resources/views/admin/overview/projects/_project.blade.php
@@ -1,11 +1,13 @@
 <div class="d-flex align-items-center justify-content-between border-bottom p-3">
-    <div class="col-3">
-        <a href="{{$project->url}}"><img class="ms-3" src="{{\Storage::url($project->image)}}" alt="{{$project->name}}" width="130"></a>
-    </div>
-    <div class="d-flex flex-column col-7">
-        <div><a href="{{$project->url}}"><b>{{$project->name}}</b></a></div>
-        <div class="mt-2 mb-2">{{$project->description}}</div>
-        <div class="text-muted">{{ ucfirst($project->start_date->translatedFormat('F Y')) }} - {{ !is_null($project->finish_date) ? ucfirst($project->finish_date->translatedFormat('F Y')) : Lang::get('admin.currently-project') }}</div>
+    <div class="col d-flex align-baseline align-items-center">
+        <div class="me-4">
+            <a href="{{$project->url}}" target="_blank"><img class="ms-3" src="{{\Storage::url($project->image)}}" alt="{{$project->name}}" width="130"></a>
+        </div>
+        <div>
+            <div><a href="{{$project->url}}"><b>{{$project->name}}</b></a></div>
+            <div class="mt-2 mb-2">{{$project->description}}</div>
+            <div class="text-muted">{{ ucfirst($project->start_date->translatedFormat('F Y')) }} - {{ !is_null($project->finish_date) ? ucfirst($project->finish_date->translatedFormat('F Y')) : Lang::get('admin.currently-project') }}</div>
+        </div>
     </div>
     <div class="col-2 d-flex text-end justify-content-end">
         <a href="#" class="me-1" title="@lang('admin.edit')"
--- resources/views/admin/overview/skills/_skill.blade.php
+++ resources/views/admin/overview/skills/_skill.blade.php
@@ -1,5 +1,7 @@
 <div class="d-flex align-items-center justify-content-between border-bottom p-3">
-    <div class="col-2 justify-content-center d-flex"><img src="{{\Storage::url($skill->image)}}" alt="{{$skill->name}}" width="75"/></div>
+    <div class="col justify-content-center d-flex">
+        <div><img src="{{\Storage::url($skill->image)}}" alt="{{$skill->name}}" width="75"/></div>
+    </div>
     <div class="d-flex flex-column col-9">
         <div class="mb-2"><b>{{$skill->name}}</b></div>
         <div class="progress">
@@ -10,7 +12,7 @@
             </div>
         </div>
     </div>
-    <div class="col d-flex text-end justify-content-end">
+    <div class="col-1 d-flex text-end justify-content-end">
         <a href="#" class="me-1" title="@lang('admin.edit')"
            data-controller="remote-modal"
            data-action="remote-modal#toggle"
