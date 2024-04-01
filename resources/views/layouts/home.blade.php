af0800f9bc16278c4ce9ce1339873f3101d26b00
Guillem Dolcet <117121662+GuillemDolcet@users.noreply.github.com>
2024-04-01 22:47:49 +0200

    Update home.blade.php

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
-    <meta name="description" content="Callum is responsive bootstrap 5 one page personal portfolio html template.">
+    <meta name="description" content="This is Guillem Dolcet Portfolio">
     <meta name="author" content="harnishdesign.net">
 
 
